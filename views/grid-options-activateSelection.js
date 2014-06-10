72578b6dacff3697107cd184e24bb1e3cbfe5fd6
globexdesigns <evgueni@globexdesigns.com>
2014-06-09 23:07:57 -0700

    Begin options documentation

---
+++ views/grid-options-activateSelection.js
@@ -0,0 +1,91 @@
+define([
+	'underscore',
+	'text!templates/grid-docs.html',
+	'text!pages/grid-options-activateSelection.html',
+	'dobygrid',
+	'js/sample-data'
+], function (_, template, page, DobyGrid, data) {
+	"use strict";
+	
+	return Backbone.DobyView.extend({
+		
+		events: {
+			"change #option-value":		"changeOption"
+		},
+		
+		initialize: function () {
+			var html = _.template(template, {
+				page: page
+			});
+			
+			this.$el.append(html);
+		},
+		
+		render: function () {
+			var columns = [{
+				id: 'id',
+				field: 'id',
+				name: 'ID',
+				removable: true,
+				width: 15
+			}, {
+				id: 'guid',
+				field: 'guid',
+				name: 'Guid',
+				removable: true
+			}, {
+				id: 'isActive',
+				field: 'isActive',
+				name: 'Is Active',
+				removable: true,
+				width: 30
+			}, {
+				id: 'balance',
+				field: 'balance',
+				name: 'Balance',
+				removable: true
+			}, {
+				id: 'age',
+				field: 'age',
+				name: 'Age',
+				removable: true,
+				width: 15
+			}, {
+				id: 'name',
+				field: 'name',
+				name: 'Name',
+				removable: true
+			}, {
+				id: 'gender',
+				field: 'gender',
+				name: 'Gender',
+				removable: true
+			}, {
+				id: 'company',
+				field: 'company',
+				name: 'Company',
+				removable: true
+			}, {
+				id: 'email',
+				field: 'email',
+				name: 'Email',
+				removable: true
+			}];
+			
+			this.grid = new DobyGrid({
+				activateSelection: true,
+				columns: columns,
+				data: data,
+				rowHeight: 35
+			}).appendTo('#demo-grid');
+		},
+		
+		changeOption: function (event) {
+			// Get value
+			var value = $(event.currentTarget).val();
+			
+			// Set value
+			this.grid.setOptions({activateSelection: value === 'false' ? false : true});
+		}
+	});
+});
