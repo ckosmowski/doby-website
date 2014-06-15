39bdda8f32fe740173b288b756d64b94cef74416
globexdesigns <evgueni@globexdesigns.com>
2014-06-14 22:01:15 -0700

    More option docs

---
+++ views/grid-options-keepNullsAtBottom.js
@@ -0,0 +1,89 @@
+define([
+	'underscore',
+	'text!templates/grid-docs.html',
+	'text!pages/grid-options-keepNullsAtBottom.html',
+	'dobygrid'
+], function (_, template, page, DobyGrid) {
+	"use strict";
+	
+	return Backbone.DobyView.extend({
+		
+		events: function () {
+			return _.extend({}, Backbone.DobyView.prototype.events, {
+				"change #option-value":		"changeOption"
+			});
+		},
+		
+		initialize: function () {
+			var html = _.template(template, {
+				page: page
+			});
+			
+			this.$el.append(html);
+		},
+		
+		render: function () {
+			var columns = [{
+				id: 'id',
+				field: 'id',
+				name: 'ID',
+				removable: true,
+				width: 15
+			}, {
+				id: 'name',
+				field: 'name',
+				name: 'Name',
+				removable: true
+			}, {
+				id: 'age',
+				field: 'age',
+				name: 'Age',
+				removable: true,
+				width: 15
+			}];
+			
+			this.grid = new DobyGrid({
+				columns: columns,
+				data: [{
+					id: 1,
+					data: {
+						id: 1,
+						name: "Abraham",
+						age: 20
+					}
+				}, {
+					id: 2,
+					data: {
+						id: 2,          
+						name: "Bobby",
+						age: 20
+					}
+				}, {
+					id: 3,
+					data: {
+						id: 3,          
+						name: null,
+						age: 21
+					}
+				}, {
+					id: 4,
+					data: {
+						id: 4,          
+						name: null,
+						age: 30
+					}
+				}],
+				rowHeight: 35
+			}).appendTo('#demo-grid');
+		},
+		
+		
+		changeOption: function (event) {
+			// Get value
+			var value = $(event.currentTarget).val();
+			
+			// Set value
+			this.grid.setOptions({keepNullsAtBottom: value === 'false' ? false : true});
+		}
+	});
+});
