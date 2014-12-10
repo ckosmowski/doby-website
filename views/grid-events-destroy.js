58632d91c82411829d0c46a17f0bda311a736ae5
Evgueni Naverniouk <globexdesigns@gmail.com>
2014-12-09 19:33:39 -0800

    Documentation for new `destroy` event.

---
+++ views/grid-events-destroy.js
@@ -0,0 +1,18 @@
+define([
+	'underscore',
+	'text!templates/grid-docs.html',
+	'text!pages/grid-events-destroy.html'
+], function (_, template, page) {
+	"use strict";
+	
+	return Backbone.DobyView.extend({
+		
+		initialize: function () {
+			var html = _.template(template, {
+				page: page
+			});
+			
+			this.$el.append(html);
+		}
+	});
+});
