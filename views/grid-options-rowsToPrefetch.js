3406066ac56664b7fadd9315f938078375f6ad54
Evgueni Naverniouk <evgueni.naverniouk@cumul8.com>
2014-12-15 14:06:23 -0800

    Add new rowsToPrefetch method and clean up the Remote Data docs page.

---
+++ views/grid-options-rowsToPrefetch.js
@@ -0,0 +1,52 @@
+define([
+	'underscore',
+	'text!templates/grid-docs.html',
+	'text!pages/grid-options-rowsToPrefetch.html',
+	'dobygrid',
+	'js/sample-fetcher'
+], function (_, template, page, DobyGrid, remotedata) {
+	"use strict";
+
+	return Backbone.DobyView.extend({
+
+		events: function () {
+			return _.extend({}, Backbone.DobyView.prototype.events, {
+				"change #option-value":		"changeOption"
+			});
+		},
+
+		initialize: function () {
+			var html = _.template(template)({page: page});
+
+			this.$el.append(html);
+		},
+
+		render: function () {
+			this.grid = new DobyGrid({
+				columns: [{
+					id: "id",
+					name: "ID",
+					field: "id"
+				}, {
+					id: "name",
+					name: "Name",
+					field: "name"
+				}, {
+					id: "age",
+					name: "Age",
+					field: "age"
+				}],
+				data: remotedata
+			}).appendTo('#demo-grid');
+		},
+
+
+		changeOption: function (event) {
+			// Get value
+			var value = $(event.currentTarget).val();
+
+			// Set value
+			this.grid.setOptions({rowsToPrefetch: parseInt(value, 10)});
+		}
+	});
+});
